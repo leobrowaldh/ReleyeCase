1f9e204fe7d5b7158ebaef022a91d3030508fdcc
leobrowaldh <leobrowaldh@gmail.com>
2025-02-17 16:46:14 +0100

    removed unused files and projects

--- ReleyeCase.Mvc/Models/CustomerFormModel.cs
+++
@@ -1,9 +0,0 @@
-﻿using ReleyeCase.Data.DbModels;
-
-namespace ReleyeCase.Mvc.Models;
-
-public class CustomerFormModel
-{
-    public Customer NewCustomer { get; set; } = new Customer();
-    public Address NewAddress { get; set; } = new Address();
-}
