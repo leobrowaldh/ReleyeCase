04e883d0dd524d4d3d9ffcca1aff931e5d29d250
leobrowaldh <leobrowaldh@gmail.com>
2025-02-17 12:14:33 +0100

    error handling on controller level

--- ReleyeCase.Mvc/Controllers/HomeController.cs
+++ ReleyeCase.Mvc/Controllers/HomeController.cs
@@ -10,23 +10,39 @@ public class HomeController(ICustomerRepository _customerRepo, ILogger<HomeContr
 {
     public async Task<IActionResult> Index()
     {
-        HomeIndexViewModel model = new()
+        try
         {
-            Customers = await _customerRepo.GetCustomers()
-        };
-        return View(model);
+            HomeIndexViewModel model = new()
+            {
+                Customers = await _customerRepo.GetCustomers()
+            };
+            return View(model);
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "Error getting customers");
+            return View();
+        }
     }
 
     [HttpPost]
     public async Task<IActionResult> AddCustomer(HomeIndexViewModel model)
     {
-        if (ModelState.IsValid)
+        try
         {
-            var createdCustomer = await _customerRepo.CreateCustomer(model.NewCustomer);
-            _logger.LogInformation($"Customer {createdCustomer.CustomerId} created");
-        }
+            if (ModelState.IsValid)
+            {
+                var createdCustomer = await _customerRepo.CreateCustomer(model.NewCustomer);
+                _logger.LogInformation($"Customer {createdCustomer.CustomerId} created");
+            }
 
-        return RedirectToAction("Index");
+            return RedirectToAction("Index");
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "Error creating customer");
+            return RedirectToAction("Index");
+        }
     }
 
     public IActionResult Privacy()
